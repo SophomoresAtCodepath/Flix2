faed39e7bfc622b85a969e02e77ff9f1933d6ca6
Donie Ypon <donieypon@Rauls-MacBook-Pro-2.local>
2018-09-07 17:05:07 -0700

    Updated README and added MBProgress HUD

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,104 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by Donie Ypon on 9/3/18.
+//  Copyright © 2018 Donie Ypon. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+import MBProgressHUD
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //progress HUD
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        tableView.dataSource = self
+        tableView.rowHeight = 200
+        fetchMovies()
+        
+        activityIndicator.startAnimating()
+        activityIndicator.stopAnimating()
+        
+        //progressHUD
+        MBProgressHUD.hide(for: self.view, animated: true)
+        
+    }
+
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response , error ) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
+        return cell
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for yo/Users/donieypon/Desktop/Flix/Podfileur project
+# platform :ios, '9.0'
+
+target 'Flix' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flix
+	pod 'AlamofireImage', '~> 3.3'
+	pod 'MBProgressHUD'
+  target 'FlixTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FlixUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
