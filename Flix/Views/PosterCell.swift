6a495fcd032454046bf101ac5b7f7ed77e094c96
Donie Ypon <donieypon@Rauls-MacBook-Pro-2.local>
2018-09-13 11:40:30 -0700

    Updated Superhero viewcontroller and cell spacing

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,16 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Donie Ypon on 9/13/18.
+//  Copyright © 2018 Donie Ypon. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+}
